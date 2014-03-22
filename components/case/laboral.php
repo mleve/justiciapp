5b5bc14e83832af9ca2c9259333a7cc7711d827e
mleve <mleve@ug.uchile.cl>
2014-03-22 11:32:10 -0300

    agregadas las otras 2 causas de demo

---
+++ components/case/laboral.php
@@ -0,0 +1,178 @@
+<div class="panel panel-default">
+        <div class="panel-heading">
+            <a class="panel-title" data-parent="#panel-resBusqueda" data-toggle="collapse" href="#panel-resLaboral">
+                <div class="row clearfix">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>O-861-2014</th>
+                                <th>VILCHES CON VALLADE</th>
+                                <th>2º Juzgado de Letras del Trabajo de Santiago</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </a>
+        </div>
+        <div id="panel-resLaboral" class="panel-collapse collapse causaResult">
+            <div class="row clearfix helpRow">
+                <div class="col-md-12 column ui-sortable ">
+                    <h3>Esta causa es del tipo: <strong> algo </strong></h3>
+                    <h4> ¿Necesitas ayuda?</h4>
+                    <h4> Tenemos guías para resolver todas tus dudas, un diagrama que te explicara las fases del proceso, o puedes revisar las leyes asociadas a esta causa:</h4>
+                    <div class="col-md-4 column ui-sortable">
+                        <a id="modal-660944" href="#modal-container" 
+                           role="button" class="btn helpBtn" 
+                           data-toggle="modal" data-url="components/guides/civil/arrendamiento.php"
+                           data-title="Guía arrendamiento"><h4>Guía</h4></a>
+                        
+                    </div>
+                    <div class="col-md-4 column ui-sortable">
+                        <a id="modal-660944" href="#modal-container" 
+                           role="button" class="btn helpBtn" 
+                           data-toggle="modal" data-url="components/diagrams/arrendamiento.php"
+                           data-title="Diagrama Arrendamiento"><h4>Diagrama</h4></a>
+                        
+                    </div>
+                    <div class="col-md-4 column ui-sortable">
+                        <a id="modal-660944" href="#modal-container" role="button" class="btn helpBtn" data-toggle="modal"><h4>Leyes</h4></a>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="row clearfix">
+                <div class="col-md-6 column ui-sortable">
+                    Estado administrativo: Sin archivar
+                </div>
+                <div class="col-md-6 column ui-sortable">
+                    Proc: Ordinario 
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="col-md-6 column ui-sortable">
+                    Etapa: Ingreso
+                </div>
+                <div class="col-md-6 column ui-sortable">
+                    Estado Proceso: Tramitación
+                </div>
+            </div>
+            <br>
+            <div class="row clearfix">
+                <div class="col-md-12 column ui-sortable">
+                    <div class="panelgroup" id="panel-interno">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <a class="panel-title" data-toggle="collapse" data-parent="#panel-interno" href=".panel-notificaciones">Notificaciones</a>
+                            </div>
+                            <div class="panel-collapse collapse panel-notificaciones" >
+                                <div class="panel-body">
+                                    <table class="table table-condensed">
+                                        <thead>
+                                            <tr>
+                                                <th>Estado Notificación</th>
+                                                <th>Fecha</th>
+                                                <th>RIT</th>
+                                                <th>RUC</th>
+                                                <th>Tipo Parte</th>
+                                                <th>Nombre</th>
+                                                <th>Tramite</th>
+                                                <th>Observación Fallida</th>
+                                            </tr>
+                                        </thead>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <a class="panel-title" data-toggle="collapse" data-parent="#panel-interno" href=".panel-litigantes">Litigantes</a>
+
+                            </div>
+                            <div class="panel-collapse collapse panel-litigantes">
+                                <div class="panel-body">
+                                    <table class="table table-condensed">
+                                        <thead>
+                                            <tr>
+                                                <th>Sujeto</th>
+                                                <th>Rut</th>
+                                                <th>Persona</th>
+                                                <th>Nombre o Razón social</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Demandante</td>
+                                                <td>7446877-2</td>
+                                                <td>NATURAL</td>
+                                                <td>David Segundo Vilches Altamirano</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Abogado Demandante</td>
+                                                <td>15415035-8</td>
+                                                <td>NATURAL</td>
+                                                <td>Eduardo Antonio Vilches Fuentes (Sin acreditación)</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Demandado</td>
+                                                <td>16088320-0</td>
+                                                <td>NATURAL</td>
+                                                <td>Andrea Vallade Campos</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <a class="panel-title" data-toggle="collapse" data-parent="#panel-interno" href=".panel-historia">Historia</a>                                                            
+                            </div>
+                            <div class="panel-collapse collapse in panel-historia" >
+                                <div class="panel-body">
+                                    <table class="table table-condensed">
+                                        <thead>
+                                            <tr>
+                                                <th>Etapa</th>
+                                                <th>Trámite</th>
+                                                <th>Desc. Trámite</th>
+                                                <th>Fecha</th>
+                                                <th>Estado</th>
+                                                <th>Descargar</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Ingreso</td>
+                                                <td>Resolución</td>
+                                                <td>Previo a proveer</td>
+                                                <td>10/03/2014</td>
+                                                <td>Firmado</td>
+                                                <td><a href="components/case/docs/prevProveer.doc">descargar</a></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Ingreso</td>
+                                                <td>Escrito</td>
+                                                <td>Ingreso causa</td>
+                                                <td>07/03/2014</td>
+                                                <td>Resuelto</td>
+                                                <td><a href="components/case/docs/demLaboral.pdf" target="_blank">Descargar</a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>                                                      
+
+
+
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <button type="button" class="btn btn-primary close-cause" data-toggle="collapse" href="#panel-resLaboral" >Cerrar</button>
+            </div>
+
+        </div>
+
+    </div>
--- components/resultAll.php
+++ components/resultAll.php
@@ -1,210 +1,57 @@
 <div class="row clearfix">
+    <h1 class="text-center">Resultados encontrados</h1>
     <table class="table" id="resultsHeader">
         <thead>
             <tr>
                 <th>ROL</th>
                 <th>CARATULADO</th>
-                <th>TRIBUNAL</th>
+                <th>TRIBUNAL/CORTE</th>
             </tr>
         </thead>
     </table>
 </div>
 <div class="panel-group" id="panel-resBusqueda">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <a class="panel-title" data-parent="#panel-resBusqueda" data-toggle="collapse" href="#panel-res1">
-                <div class="panel-heading">
+    <?php include 'case/civil.php'; ?>
+    <?php include 'case/laboral.php'; ?>
+    <?php include 'case/apelaciones.php'; ?>
+</div>
+<div class="view">
+    <!-- Button to trigger modal -->
+   
+
+    <!-- Modal -->
+    <div class="modal fade" id="modal-container" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h4 class="modal-title" id="myModalLabel" ">Modal title</h4>
+                </div>
+                <div class="modal-body">
                     <div class="row clearfix">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>C-2905-2014</th>
-                                    <th>VILCHES/ARAYA</th>
-                                    <th>1° TRIBUNAL CIVIL</th>
-                                </tr>
-                            </thead>
-                        </table>
+                        <div class="col-md-12" id="modalBody">
+                            
+                        </div>
                     </div>
                 </div>
-            </a>
-        </div>
-        <div id="panel-res1" class="panel-collapse collapse">
-            <div class="row clearfix">
-                <div class="col-md-12 column ui-sortable">
-                    <div class="panelgroup" id="panel-interno">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <a class="panel-title" data-toggle="collapse" data-parent="#panel-res1" href=".panel-notificaciones">Notificaciones</a>
-                            </div>
-                            <div class="panel-collapse collapse panel-notificaciones" >
-                                <div class="panel-body">
-                                    <table class="table table-condensed">
-                                        <thead>
-                                            <tr>
-                                                <th>Estado Notificación</th>
-                                                <th>Fecha Tramite</th>
-                                                <th>Tipo Parte</th>
-                                                <th>Nombre</th>
-                                                <th>Tramite</th>
-                                                <th>Observación Fallida</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>TB - Monthly</td>
-                                                <td>01/04/2012</td>
-                                                <td>Default</td>
-                                            </tr>
-                                            <tr class="active">
-                                                <td>1</td>
-                                                <td>TB - Monthly</td>
-                                                <td>01/04/2012</td>
-                                                <td>Approved</td>
-                                            </tr>
-                                            <tr class="success">
-                                                <td>2</td>
-                                                <td>TB - Monthly</td>
-                                                <td>02/04/2012</td>
-                                                <td>Declined</td>
-                                            </tr>
-                                            <tr class="warning">
-                                                <td>3</td>
-                                                <td>TB - Monthly</td>
-                                                <td>03/04/2012</td>
-                                                <td>Pending</td>
-                                            </tr>
-                                            <tr class="danger">
-                                                <td>4</td>
-                                                <td>TB - Monthly</td>
-                                                <td>04/04/2012</td>
-                                                <td>Call in to confirm</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <a class="panel-title" data-toggle="collapse" data-parent="#panel-res1" href=".panel-litigantes">Litigantes</a>
-
-                            </div>
-                            <div class="panel-collapse collapse panel-litigantes">
-                                <div class="panel-body">
-                                    <table class="table table-condensed">
-                                        <thead>
-                                            <tr>
-                                                <th>Estado Notificación</th>
-                                                <th>Fecha Tramite</th>
-                                                <th>Tipo Parte</th>
-                                                <th>Nombre</th>
-                                                <th>Tramite</th>
-                                                <th>Observación Fallida</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>TB - Monthly</td>
-                                                <td>01/04/2012</td>
-                                                <td>Default</td>
-                                            </tr>
-                                            <tr class="active">
-                                                <td>1</td>
-                                                <td>TB - Monthly</td>
-                                                <td>01/04/2012</td>
-                                                <td>Approved</td>
-                                            </tr>
-                                            <tr class="success">
-                                                <td>2</td>
-                                                <td>TB - Monthly</td>
-                                                <td>02/04/2012</td>
-                                                <td>Declined</td>
-                                            </tr>
-                                            <tr class="warning">
-                                                <td>3</td>
-                                                <td>TB - Monthly</td>
-                                                <td>03/04/2012</td>
-                                                <td>Pending</td>
-                                            </tr>
-                                            <tr class="danger">
-                                                <td>4</td>
-                                                <td>TB - Monthly</td>
-                                                <td>04/04/2012</td>
-                                                <td>Call in to confirm</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <a class="panel-title" data-toggle="collapse" data-parent="#panel-res1" href=".panel-historia">Historia</a>                                                            
-                            </div>
-                            <div class="panel-collapse collapse panel-historia" >
-                                <div class="panel-body">
-                                    <table class="table table-condensed">
-                                        <thead>
-                                            <tr>
-                                                <th>Etapa</th>
-                                                <th>Trámite</th>
-                                                <th>Desc. Trámite</th>
-                                                <th>Fecha</th>
-                                                <th>Descargar</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>Ingreso</td>
-                                                <td>Resolución</td>
-                                                <td>Apercibimientos articulo 2 ley 18.120</td>
-                                                <td>10/03/2014</td>
-                                                <td> descargar <td>
-                                            </tr>
-                                            <tr>
-                                                <td>Ingreso</td>
-                                                <td>Escrito</td>
-                                                <td>Ingreso demanda</td>
-                                                <td>07/03/2014</td>
-                                                <td>Descargar</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>                                                      
-
-
-
-
-                    </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
                 </div>
-            </div>
-            <div class="row clearfix">
-                <div class="col-md-6 column ui-sortable">
-                    Estado administrativo: Si
-                </div>
-                <div class="col-md-6 column ui-sortable">
-                    Proc: Sumario Especial 
-                </div>
-            </div>
-            <div class="row clearfix">
-                <div class="col-md-6 column ui-sortable">
-                    Etapa: 0 ingreso
-                </div>
-                <div class="col-md-6 column ui-sortable">
-                    Estado Proceso: Tramitación
-                </div>
-            </div>
-            <div>
-                <button type="button" class="btn btn-primary" data-toggle="collapse" href="#panel-res1" >Cerrar</button>
-            </div>
-        </div>
-
-    </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 
 </div>
-
-
+<script type="text/javascript">
+   $('.helpBtn').on('click',function(event){
+       event.preventDefault();
+       var url = $(this).attr('data-url');
+       $('.modal-title').html($(this).attr('data-title'));
+       $.ajax(url)
+               .done(function(response){
+                   $('#modalBody').html(response);
+                   $(this).unbind('click');
+               });
+       
+   }); 
+</script>
