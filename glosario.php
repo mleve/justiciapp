91e94281fcc16be085409aa0973e0676d852d587
mleve <mleve@ug.uchile.cl>
2014-03-22 14:44:25 -0300

    agregado glosario

---
+++ glosario.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>NOMBREAPLICACION</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+        <!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+        <!--script src="js/less-1.3.3.min.js"></script-->
+        <!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="js/html5shiv.js"></script>
+        <![endif]-->
+
+        <!-- Fav and touch icons -->
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png">
+        <link rel="shortcut icon" href="img/favicon.png">
+
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/scripts.js"></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <?php include('components/header.php'); ?>
+            <div class="row clearfix">
+                <div class="col-md-12 column">
+                    <div class="tabbable" id="tabs-498981"> <!-- Only required for left/right tabs -->
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#panel-a" data-toggle="tab" >A</a></li>
+                            <li class=""><a href="#panel-b" data-toggle="tab" >B</a></li>
+                            <li class=""><a href="#panel-c" data-toggle="tab" >C</a></li>
+                            <li class=""><a href="#panel-d" data-toggle="tab" >D</a></li>
+                            <li class=""><a href="#panel-e" data-toggle="tab" >E</a></li>
+                            <li class=""><a href="#panel-f" data-toggle="tab" >F</a></li>
+                            <li class=""><a href="#panel-g" data-toggle="tab" >G</a></li>
+                            <li class=""><a href="#panel-h" data-toggle="tab" >H</a></li>
+                            <li class=""><a href="#panel-i" data-toggle="tab" >I</a></li>
+                            <li class=""><a href="#panel-j" data-toggle="tab" >J</a></li>
+                            <li class=""><a href="#panel-k" data-toggle="tab" >K</a></li>
+                            <li class=""><a href="#panel-l" data-toggle="tab" >L</a></li>
+                            <li class=""><a href="#panel-l" data-toggle="tab" >M</a></li>
+                            <li class=""><a href="#panel-l" data-toggle="tab" >N</a></li>
+                            <li class=""><a href="#panel-l" data-toggle="tab" >P</a></li>
+                            <li class=""><a href="#panel-l" data-toggle="tab" >Q</a></li>
+                            <li class=""><a href="#panel-l" data-toggle="tab" >R</a></li>
+                            <li class=""><a href="#panel-l" data-toggle="tab" >S</a></li>
+                            <li class=""><a href="#panel-l" data-toggle="tab" >T</a></li>
+                            <li class=""><a href="#panel-l" data-toggle="tab" >U</a></li>
+                            <li class=""><a href="#panel-l" data-toggle="tab" >V</a></li>
+                            <li class=""><a href="#panel-l" data-toggle="tab" >W</a></li>
+                            <li class=""><a href="#panel-l" data-toggle="tab" >X Y Z</a></li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="panel-a">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Concepto</th>
+                                            <th>Explicación</th>                                           
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Absolución</td>
+                                            <td>Resolución judicial en que al acusado se le declara inocente de los cargos formulados,
+                                                determinando que la persona no tiene responsabilidad legal por los crímenes que se le imputaban.</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Acción </td>
+                                            <td>El derecho de las personas a traer una reivindicación ante las cortes para obtener satisfacción. En el antiguo sistema penal, es ejercido por el Estado a través del rol investigativo y punitivo del juez (ahora, la investigación la realiza el fiscal).</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Agravantes </td>
+                                            <td>Circunstancias que agravan la pena normal asignada a un delito. Incluyen alevosía, recompensa, premeditación, abuso de confianza y reincidencia. Ver art. 12 del Código Penal.</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Alevosía</td>
+                                            <td>Agravante de la responsabilidad penal, que puede provocar una pena más alta. Consiste en 1. "Obrar a sobreseguro" (crear un estado de indefensión y aprovecharse de ello) y 2. "Obrar a traición"(aprovecharse de ganar la confianza de la víctima).</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Amparo </td>
+                                            <td>Recurso para proteger la integridad física de una persona detenida, también llamado "habeas corpus". Un juez debe fiscalizar que la persona no esté siendo maltratada.   </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Apelación </td>
+                                            <td>Acción de una parte por estimar que una sentencia judicial es errada o no se ajusta al derecho. Se envía la sentencia a una corte superior, que la revisa para luego confirmarla o corregirla.</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Apelado</td>
+                                            <td>"Sujeto pasivo" de un recurso de apelación significa la persona que obtuvo una sentencia favorable, con la cual la contraparte estuvo en desacuerdo y por la cual interpuso un recurso de apelación.</td>
+
+                                        </tr>
+                                        <tr>
+                                            <td>Apelante</td>
+                                            <td>"Sujeto activo" del recurso de apelación persona que interpuso el recurso por estar en desacuerdo con una sentencia definitiva o interlocutoria dictado en su causa. </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Apremios Ilegítimos  </td>
+                                            <td>Crimen consistente en la aplicación de tormentos o cualquier trato cruel, inhumano y degradante, sea físico o psicológico. Equivalente en el código chileno al crimen internacional de tortura. Tiene penas estipuladas en art 150 del Código Penal.</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Amparo </td>
+                                            <td>Recurso para proteger la integridad física de una persona detenida, también llamado "habeas corpus". Un juez debe fiscalizar que la persona no esté siendo maltratada.   </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Atenuantes</td>
+                                            <td>Circunstancias que disminuyen la pena normal aplicable a un crimen. Incluyen "irreprochable conducta anterior" y "obrar por estímulos poderosos". Ver art 11 del Código Penal.</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Autor</td>
+                                            <td>Persona con responsabilidad por un crimen. Incluye (1) Tomar parte, directamente o impidiendo o procurando impedir que se evite (2) Forzar o inducir directamente a otro a ejecutarlo (3) Facilitar los medios o presenciar. Ver art 15 del Código Penal.</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane" id="panel-b">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <th>Concepto</th>
+                                        <th>Explicación</th>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Buena fe</td>
+                                            <td>Principio jurídico que consiste en la convicción de que se esta actuando conforme a derecho y que el comportamiento se ajusta a cierto estándar de conducta. </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Bajo apercibimiento</td>
+                                            <td>Expresión judicial que advierte la aplicación de una sanción por no realizar una obligación dispuesta en una citación, notificación o mandato judicial.  </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Bien</td>
+                                            <td>Dícese de todo aquello que tiene una medida de valor y puede ser objeto de protección jurídica.  </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Bien inalienable</td>
+                                            <td>Que no puede ser objeto de venta.  </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Bienes indivisos</td>
+                                            <td>Son los que pertenecen a dos o más personas y se encuentran en imposibilidad de individualizarse literalmente para cada uno de los condóminos, por imposición de la ley, acuerdo de las partes o acto de última voluntad.  </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Bienes inembargables</td>
+                                            <td>Bienes que el deudor conserva, por encontrarse fuera de la ejecución y exentos de toda medida de seguridad. Tiene servicio prohibitivo expreso, es de carácter restrictivo y ha de estar dispuesto por ley su exclusión por ley.  </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Bilateralidad</td>
+                                            <td>Lo que consta de dos lados. En derecho se aplica a los contratos en que ambas partes quedan obligadas a dar, hacer o no hacer que compensa la prestación de la otra parte con mayor o menor igualdad.    </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane" id="panel-c">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <th>Concepto</th>
+                                        <th>Explicación</th>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane" id="panel-d">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <th>Concepto</th>
+                                        <th>Explicación</th>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane" id="panel-e">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <th>Concepto</th>
+                                        <th>Explicación</th>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane" id="panel-f">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <th>Concepto</th>
+                                        <th>Explicación</th>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane" id="panel-g">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <th>Concepto</th>
+                                        <th>Explicación</th>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane" id="panel-h">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <th>Concepto</th>
+                                        <th>Explicación</th>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane" id="panel-i">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <th>Concepto</th>
+                                        <th>Explicación</th>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane" id="panel-j">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <th>Concepto</th>
+                                        <th>Explicación</th>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane" id="panel-k">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <th>Concepto</th>
+                                        <th>Explicación</th>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane" id="panel-l">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <th>Concepto</th>
+                                        <th>Explicación</th>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>		
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
